eef4c31907826dd522f3eb6fa6b71ca3f2069e92
Nicolas <briantgomez2001@gmail.com>
2022-03-23 11:13:55 -0300

    y ahora que

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (i==1) {
+            system.out.println("nose");
+        }
     }
 }
